8f32da47a15652db97ce02c70792e5ae074f21a3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-01-21 20:03:49 +0100

    Added formgroup and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'brewbrah';
+export class AppComponent implements OnInit{
+
+  public form = new FormGroup({
+    wantedAmountMl: new FormControl(1000),
+    waterNeeded: new FormControl(1115),
+    coffeeGram: new FormControl(60),
+    brewRatio: new FormControl(17),
+    wastedWaterPerGramCoffee: new FormControl(2)
+  });
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -22,7 +22,9 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     NgZorroAntdModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    NzIconModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
