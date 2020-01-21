7e1267c0b8fc1b80a4c872a93f44f5816695f2d3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-01-21 20:54:30 +0100

    Math bits done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,40 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   public form = new FormGroup({
-    wantedAmountMl: new FormControl(1000),
-    waterNeeded: new FormControl(1115),
-    coffeeGram: new FormControl(60),
+    desiredAmount: new FormControl(1234),
+    waterNeeded: new FormControl(1000),
+    coffeeNeeded: new FormControl(42),
     brewRatio: new FormControl(17),
-    wastedWaterPerGramCoffee: new FormControl(2)
+    waterAbsorbed: new FormControl(2)
   });
 
   ngOnInit(): void {
+    this.form.controls.desiredAmount.valueChanges.subscribe(desired => {
+      const ratio = this.form.controls.brewRatio.value;
+      this.form.controls.coffeeNeeded.setValue(desired / ratio);
+    });
+
+    this.form.controls.coffeeNeeded.valueChanges.subscribe(coffeeNeeded => {
+      const desired = this.form.controls.desiredAmount.value;
+      const absorbed = this.form.controls.waterAbsorbed.value;
+      this.form.controls.waterNeeded.setValue(desired + coffeeNeeded * absorbed);
+    });
+
+    this.form.controls.brewRatio.valueChanges.subscribe(ratio => {
+      const desired = this.form.controls.desiredAmount.value;
+      this.form.controls.coffeeNeeded.setValue(desired / ratio);
+    });
+
+    this.form.controls.waterAbsorbed.valueChanges.subscribe(absorbed => {
+      const coffee = this.form.controls.coffeeNeeded.value;
+      const desired = this.form.controls.desiredAmount.value;
+      this.form.controls.waterNeeded.setValue(desired + coffee * absorbed);
+    });
+
+    this.form.controls.desiredAmount.setValue(1000);
   }
 
 }
