278de031d5a46c1a83c851990a440f48c16331ab
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-01-22 18:04:00 +0100

    Changed number of used decimals to one place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,24 +21,24 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.form.controls.desiredAmount.valueChanges.subscribe(desired => {
       const ratio = this.form.controls.brewRatio.value;
-      this.form.controls.coffeeNeeded.setValue(desired / ratio);
+      this.form.controls.coffeeNeeded.setValue(AppComponent.roundToDecimals(desired / ratio));
     });
 
     this.form.controls.coffeeNeeded.valueChanges.subscribe(coffeeNeeded => {
       const desired = this.form.controls.desiredAmount.value;
       const absorbed = this.form.controls.waterAbsorbed.value;
-      this.form.controls.waterNeeded.setValue(desired + coffeeNeeded * absorbed);
+      this.form.controls.waterNeeded.setValue(AppComponent.roundToDecimals(desired + coffeeNeeded * absorbed));
     });
 
     this.form.controls.brewRatio.valueChanges.subscribe(ratio => {
       const desired = this.form.controls.desiredAmount.value;
-      this.form.controls.coffeeNeeded.setValue(desired / ratio);
+      this.form.controls.coffeeNeeded.setValue(AppComponent.roundToDecimals(desired / ratio));
     });
 
     this.form.controls.waterAbsorbed.valueChanges.subscribe(absorbed => {
       const coffee = this.form.controls.coffeeNeeded.value;
       const desired = this.form.controls.desiredAmount.value;
-      this.form.controls.waterNeeded.setValue(desired + coffee * absorbed);
+      this.form.controls.waterNeeded.setValue(AppComponent.roundToDecimals(desired + coffee * absorbed));
     });
 
     this.form.controls.desiredAmount.setValue(1000);
@@ -47,5 +47,10 @@ export class AppComponent implements OnInit {
   public goToSource(): void {
     this.goToSourceClicked = true;
     window.location.href = 'https://github.com/bjornnorgaard/brewbrah';
+  }
+
+  private static roundToDecimals(num: number, decimals: number = 0): number {
+    const roundTo = 10 ^ decimals;
+    return Math.round(num * (roundTo)) / roundTo;
   }
 }
