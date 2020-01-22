dbe625285dd65d2de77bd59b4cb862f035ad10c9
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-01-22 21:28:12 +0100

    Added firebase config to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCvcfJpQUK7B0n5kg_mzO-Bgz0CglgZJZU",
+    authDomain: "brewbrah.firebaseapp.com",
+    databaseURL: "https://brewbrah.firebaseio.com",
+    projectId: "brewbrah",
+    storageBucket: "brewbrah.appspot.com",
+    messagingSenderId: "831434026505",
+    appId: "1:831434026505:web:b7edc990345f7cdc8ed2d8",
+    measurementId: "G-3X60GP1YKW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCvcfJpQUK7B0n5kg_mzO-Bgz0CglgZJZU",
+    authDomain: "brewbrah.firebaseapp.com",
+    databaseURL: "https://brewbrah.firebaseio.com",
+    projectId: "brewbrah",
+    storageBucket: "brewbrah.appspot.com",
+    messagingSenderId: "831434026505",
+    appId: "1:831434026505:web:b7edc990345f7cdc8ed2d8",
+    measurementId: "G-3X60GP1YKW"
+  }
 };
 
 /*
