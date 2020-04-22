97624d46016d54390275d8b7732624dd10458035
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-04-22 21:21:51 +0200

    Added splash screen

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:brewbrah/home_material.dart';
 import 'package:flutter/material.dart';
 
@@ -7,11 +9,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeMaterial());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/HomeScreen': (BuildContext context) => HomeMaterial()
+      },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/ic_brewbrah/res/mipmap-xxxhdpi/ic_brewbrah.png'),
+      ),
+    );
   }
 }
